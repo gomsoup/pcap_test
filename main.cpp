78b075eb3b55b64d7ba6bc086a45333f2d10ccf2
gomsoup <gomsoup@gmail.com>
2017-07-17 15:25:09 +0900

    Net, MASK print added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
-#include <pcap.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <pcap.h>
+
+#include <errno.h>
+
+#include <sys/time.h>
+#include <sys/socket.h>
+
+#include <net/ethernet.h>
+
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip_icmp.h>
+
+#include <arpa/inet.h>
+
+#define PROMISCUOUS 1
+#define NONPRIMISCUOUS 0
+
+using namespace std;
+
+
+struct ip *iph; // ip header structure
+struct tcphdr *tcph; //tcp header structure
 
 class pcapClass{
 public:
@@ -45,11 +72,31 @@ public:
 		}
 	}
 
-	void pcapGetPacket(){
-		packet = pcap_next(handle, &header);
-		printf("Jacked a packet with legth of [%d]\n", header.len);
+	void pcapPrintNet(){
+		char *netp;
+		struct in_addr addr;
+
+		addr.s_addr = net;
+		netp = inet_ntoa(addr);
+
+		cout << "NET : " << netp << endl;
+	}
+	
+	void pcapPrintMask(){
+		char *maskp;
+		struct in_addr addr;
+
+		addr.s_addr = mask;
+		maskp = inet_ntoa(addr);
+
+		cout << "MASK : " << maskp << endl;
 	}
 
+	void pcapGetPacket(){
+		packet = pcap_next(handle, &header);
+		cout << "Jacked a packet with legnth of [" << header.len << "]" << endl;
+	}
+	
 	pcapClass(){
 		defineTheDevice();
 		propertiesForDevice();
@@ -64,7 +111,9 @@ public:
 
 int main(){
 	pcapClass p;
-	
+
 	p.pcapGetPacket();
+	p.pcapPrintNet();
+	p.pcapPrintMask();
 	return 0;
 }
